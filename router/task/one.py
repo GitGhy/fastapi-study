7ff3e6f6fc4f1e722c92b57bfb408068f6f16891
zzzp <2399299188@qq.com>
2023-11-30 09:58:29 +0800

    one文件添加print

--- router/task/one.py
+++ router/task/one.py
@@ -8,6 +8,6 @@ router = APIRouter(prefix="/task",
 @router.get("/one")
 async def read_users():
     print("as")
-    print("bs")
+    print("cs")
     return [{"username": "g"}, {"username": "?"}]
 
