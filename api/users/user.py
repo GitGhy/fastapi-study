8edb87a31db1fa6cb30791b9667373fb1a9cae49
ghy <10687980+Ghyan@user.noreply.gitee.com>
2023-12-17 01:34:42 +0800

    注入依赖

--- api/users/user.py
+++ api/users/user.py
@@ -1,10 +1,50 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Request, HTTPException
+
 
 router = APIRouter(prefix="/user",
                    tags=["user"],
                    responses={404: {"description": "Not found"}}, )
 
 
-@router.get("/{name}")
-async def read_users(name: str):
-    return [{"username": name}]
+class Req:
+    def __init__(self, request: Request):
+        self.request = request
+
+
+class UserInfoVerify:
+    def __init__(self, request: Request):
+        self.request = request
+
+    @staticmethod
+    async def verification(name: str, age: int):
+        if not name or not age:
+            return False, {
+                "code": 400,
+                "success": False,
+                "msg": "参数类型为空",
+            }
+        if not isinstance(name, str) or not isinstance(age, int):
+            return False, {
+                "code": 401,
+                "success": False,
+                "msg": "参数类型错误",
+            }
+        return True, "success"
+
+
+@router.post("/userinfo")
+async def read_userinfo(unv: UserInfoVerify = Depends()):
+    userinfo = await unv.request.json()
+    print(userinfo)
+    name = userinfo.get('name')
+    age = userinfo.get('age')
+    is_valid, msg = await unv.verification(name, age)
+    if not is_valid:
+        raise HTTPException(status_code=400, detail=msg)
+    return_data = [{
+        "userinfo": {
+            'name': userinfo['name'],
+            'age': userinfo['age']
+        }
+    }]
+    return return_data
--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import traceback
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from starlette.responses import JSONResponse
 from fastapi.routing import RequestValidationError
 from api.users import user
 from api.task import one
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 
 app = FastAPI(title="FastAPI",
               description="FastAPI文档",
               version="0.0.1")
-
-# 添加 CORS 中间件以允许跨域请求
+# 添加中间件
+# 允许跨域请求
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["*"],  # 允许所有来源
+    allow_credentials=True,  # 允许证书
+    allow_methods=["*"],  # 允许所有请求方法
+    allow_headers=["*"],  # 允许所有请求头
+    expose_headers=["*"],  # 允许暴露所有头
+    max_age=600  # 请求的缓存时间
+)
+# 压缩数据
+app.add_middleware(
+    GZipMiddleware,
+    minimum_size=1024,  # 压缩大于1024字节的数据
 )
 
 
@@ -59,7 +68,11 @@ async def root():
 
 
 @app.get("/hello/{name}", tags=['app实例对象注册接口'])
-async def say_hello(name: int):
+async def say_hello(name: int | str):
+    if type(name) == str:
+        raise HTTPException(status_code=400, detail={
+            "code": 400,
+        })
     return {"message": f"Hello {name}"}
 
 
